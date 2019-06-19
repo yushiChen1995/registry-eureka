0f6389162bed17afdcd3d314b7dd8baf9d1a21e8
chenyushi <chenyushi@lechebang.com>
2019-06-19 15:23:13 +0800

    注册中心

---
+++ src/main/java/com/cys/Test.java
@@ -0,0 +1,25 @@
+package com.cys;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author chenyushi
+ * @date 2019/4/15 10:49
+ */
+public class Test implements ResponseBodyAdvice {
+
+
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class aClass) {
+        return false;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        return null;
+    }
+}
